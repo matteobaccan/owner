c31afd5b7df44ae1511481d35f262af8a0f2fd1d
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2012-12-28 01:20:15 +0100

    new unit test

---
+++ src/test/java/owner/SystemVariableExpanderTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2012, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package owner;
+
+import org.junit.Test;
+
+import static java.lang.System.getProperty;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Luigi R. Viggiano
+ */
+public class SystemVariableExpanderTest {
+    @Test
+    public void testExpandMultipleTilde() {
+        SystemVariablesExpander expander = new SystemVariablesExpander();
+        String result = expander.expand("foo-~-bar-~-baz");
+        String expected = "foo-" + getProperty("user.home") + "-bar-" + getProperty("user.home") + "-baz";
+        assertEquals(expected, result);
+    }
+}
