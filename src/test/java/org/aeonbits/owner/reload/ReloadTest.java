f12a3a1b2415b311f17946136bdb5b66b21c67b8
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-06-05 15:20:40 +0200

    an interesting test

--- src/test/java/org/aeonbits/owner/reload/ReloadTest.java
+++ src/test/java/org/aeonbits/owner/reload/ReloadTest.java
@@ -59,6 +59,22 @@ public class ReloadTest {
         assertEquals(Integer.valueOf(21), cfg.minimumAge());
     }
 
+    @Test
+    public void testReloadWithImportedProperties() throws Throwable {
+        Properties props = new Properties() {{
+           setProperty("minimumAge", "18");
+        }};
+
+        ReloadableConfig cfg = ConfigFactory.create(ReloadableConfig.class, props);
+        assertEquals(Integer.valueOf(18), cfg.minimumAge());
+
+        props.setProperty("minimumAge", "21"); // changing props doesn't reflect to cfg immediately
+        assertEquals(Integer.valueOf(18), cfg.minimumAge());
+
+        cfg.reload(); // the config gets reloaded, so the change in props gets reflected
+        assertEquals(Integer.valueOf(21), cfg.minimumAge());
+    }
+
     private void save(Properties p) throws Throwable {
         target.getParentFile().mkdirs();
         p.store(new FileWriter(target), "reloadable config example");
