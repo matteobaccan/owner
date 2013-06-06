ca3eb9a038fdea3f108d4406c244cd232979ce7f
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-06-06 23:58:00 +0200

    refactoring

---
+++ src/main/java/org/aeonbits/owner/DelegatedMethods.java
@@ -0,0 +1,69 @@
+package org.aeonbits.owner;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+enum DelegatedMethods {
+    LIST_PRINT_STREAM(getMethod(Listable.class, "list", PrintStream.class)) {
+        @Override
+        public Object invoke(PropertiesManager propsMgr, Object... args) {
+            propsMgr.list((PrintStream) args[0]);
+            return null;
+        }
+    },
+
+    LIST_PRINT_WRITER(getMethod(Listable.class, "list", PrintWriter.class)) {
+        @Override
+        public Object invoke(PropertiesManager propsMgr, Object... args) {
+            propsMgr.list((PrintWriter) args[0]);
+            return null;
+        }
+    },
+
+    RELOAD(getMethod(Reloadable.class, "reload")) {
+        @Override
+        public Object invoke(PropertiesManager propsMgr, Object... args) {
+            propsMgr.reload();
+            return null;
+        }
+    },
+
+    SET_PROPERTY(getMethod(Modifiable.class, "setProperty", String.class, String.class)) {
+        @Override
+        public Object invoke(PropertiesManager propsMgr, Object[] args) {
+            return propsMgr.setProperty((String) args[0], (String) args[1]);
+        }
+    },
+
+    REMOVE_PROPERTY(getMethod(Modifiable.class, "removeProperty", String.class)) {
+        @Override
+        public Object invoke(PropertiesManager propsMgr, Object[] args) {
+            return propsMgr.removeProperty((String) args[0]);
+        }
+    };
+
+    private final Method proxiedMethod;
+
+    DelegatedMethods(Method proxiedMethod) {
+        this.proxiedMethod = proxiedMethod;
+    }
+
+    boolean matches(Method proxy) {
+        return proxiedMethod != null && proxiedMethod.getName().equals(proxy.getName())
+                && Arrays.equals(proxiedMethod.getParameterTypes(), proxy.getParameterTypes());
+    }
+
+    private static Method getMethod(Class<?> aClass, String name, Class<?>... args) {
+        try {
+            return aClass.getMethod(name, args);
+        } catch (NoSuchMethodException e) {
+            // this shouldn't happen, btw we handle the case in which the delegate method is not available...
+            // so, it's fine.
+            return null;
+        }
+    }
+
+    abstract Object invoke(PropertiesManager propsMgr, Object[] args);
+}
--- src/main/java/org/aeonbits/owner/PropertiesInvocationHandler.java
+++ src/main/java/org/aeonbits/owner/PropertiesInvocationHandler.java
@@ -39,70 +39,7 @@ class PropertiesInvocationHandler implements InvocationHandler {
     private final StrSubstitutor substitutor;
     private final PropertiesManager propertiesManager;
 
-    private enum DelegatedMethods {
-        LIST_PRINT_STREAM(getMethod(Listable.class, "list", PrintStream.class)) {
-            @Override
-            public Object invoke(PropertiesManager propsMgr, Object... args) {
-                propsMgr.list((PrintStream)args[0]);
-                return null;
-            }
-        },
 
-        LIST_PRINT_WRITER(getMethod(Listable.class, "list", PrintWriter.class)) {
-            @Override
-            public Object invoke(PropertiesManager propsMgr, Object... args) {
-                propsMgr.list((PrintWriter)args[0]);
-                return null;
-            }
-        },
-
-        RELOAD(getMethod(Reloadable.class, "reload")) {
-            @Override
-            public Object invoke(PropertiesManager propsMgr, Object... args) {
-                propsMgr.reload();
-                return null;
-            }
-        },
-
-        SET_PROPERTY(getMethod(Modifiable.class, "setProperty", String.class, String.class)) {
-            @Override
-            public Object invoke(PropertiesManager propsMgr, Object[] args) {
-                return propsMgr.setProperty((String)args[0], (String)args[1]);
-            }
-        },
-
-        REMOVE_PROPERTY(getMethod(Modifiable.class, "removeProperty", String.class)) {
-            @Override
-            public Object invoke(PropertiesManager propsMgr, Object[] args) {
-                return propsMgr.removeProperty((String)args[0]);
-            }
-        }
-
-        ;
-
-        final Method proxiedMethod;
-
-        DelegatedMethods(Method proxiedMethod) {
-            this.proxiedMethod = proxiedMethod;
-        }
-
-        private boolean matches(Method proxy) {
-            return proxiedMethod != null && proxiedMethod.getName().equals(proxy.getName())
-                    && Arrays.equals(proxiedMethod.getParameterTypes(), proxy.getParameterTypes());
-        }
-
-        private static Method getMethod(Class<?> aClass, String name, Class<?>... args) {
-            try {
-                return aClass.getMethod(name, args);
-            } catch (NoSuchMethodException e) {
-                // this shouldn't happen, btw we handle the case in which the delegate method is not available...
-                // so, it's fine.
-                return null;
-            }
-        }
-
-        public abstract Object invoke(PropertiesManager propsMgr, Object[] args);
-    }
 
     PropertiesInvocationHandler(PropertiesManager manager) {
         this.propertiesManager = manager;
