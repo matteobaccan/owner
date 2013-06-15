3c49401b252d4968564c92f5a296feacd8f9b2f9
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-06-15 20:08:09 +0200

    test auto reload when the configuration properties file is contained inside a jar file... ;-)

--- src/test/java/org/aeonbits/owner/autoreload/AutoReloadTest.java
+++ src/test/java/org/aeonbits/owner/autoreload/AutoReloadTest.java
@@ -18,11 +18,17 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.aeonbits.owner.UtilTest.save;
@@ -34,13 +40,21 @@ import static org.junit.Assert.assertTrue;
  */
 public class AutoReloadTest {
 
-    private static final String spec = "file:target/test-resources/AutoReloadConfig.properties";
+    private static final String propertyFileName = "AutoReloadConfig.properties";
+    private static final String jarFile = "target/test-resources/AutoReloadExample.jar";
+
+    private static final String spec = "file:target/test-resources/" + propertyFileName;
+    private static final String specJar = "jar:file:" + jarFile + "!/" + propertyFileName;
+
     private static File target;
+    private static File jarTarget;
+
     private static FakeTime time;
 
     @BeforeClass
     public static void beforeClass() throws MalformedURLException {
         target = new File(new URL(spec).getFile());
+        jarTarget = new File(jarFile);
     }
 
     @Before
@@ -72,10 +86,62 @@ public class AutoReloadTest {
         }});
 
         time.elapse(4, SECONDS);                             // make 4 seconds elapse.
-        assertEquals(Integer.valueOf(10), cfg.someValue());  // the change is not reflected yet since interval is 5 secs.
+        assertEquals(Integer.valueOf(10), cfg.someValue());  // change is not reflected yet since interval is 5 secs.
 
         time.elapse(1, SECONDS);                             // another second is elapsed
         assertEquals(Integer.valueOf(20), cfg.someValue());  // the changed file should be reloaded now.
+    }
+
+    @Sources(specJar)
+    @HotReload(interval = 5, unit = SECONDS)
+    interface AutoReloadJarConfig extends Config {
+        Integer someValue();
+    }
+
+    @Test
+    public void testAutoReloadOnJarFile() throws Throwable {
+        writeJarFile(jarTarget, propertyFileName,
+                new Properties() {{
+                    setProperty("someValue", "10");
+                }});
+
+        boolean success = jarTarget.setLastModified(jarTarget.lastModified() - 15000); // make the file 15 seconds older
+        assertTrue(success);
+
+        time.setTime(jarTarget.lastModified());              // set the time for this test to match the file creation
+
+        AutoReloadJarConfig cfg = ConfigFactory.create(AutoReloadJarConfig.class);
+        assertEquals(Integer.valueOf(10), cfg.someValue());
+
+        writeJarFile(jarTarget, propertyFileName,    // file updated, the current time is set in target.lastModified().
+                new Properties() {{
+                    setProperty("someValue", "20");
+                }});
+
+        time.elapse(4, SECONDS);                             // make 4 seconds elapse.
+        assertEquals(Integer.valueOf(10), cfg.someValue());  // change is not reflected yet since interval is 5 secs.
+
+        time.elapse(1, SECONDS);                             // another second is elapsed
+        assertEquals(Integer.valueOf(20), cfg.someValue());  // the changed file should be reloaded now.
+    }
+
+    private void writeJarFile(File file, String entryName, Properties props) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        props.store(out, "saved for test");
+        InputStream input = new ByteArrayInputStream(out.toByteArray());
+        JarOutputStream output = new JarOutputStream(
+                new FileOutputStream(file));
+        try {
+            ZipEntry entry = new ZipEntry(entryName);
+            output.putNextEntry(entry);
+            byte[] buffer = new byte[4096];
+            int size;
+            while ((size = input.read(buffer)) != -1)
+                output.write(buffer, 0, size);
+        } finally {
+            input.close();
+            output.close();
+        }
     }
 
     @After
