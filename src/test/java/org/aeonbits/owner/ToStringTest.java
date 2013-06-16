03055f7baa11ba7f6bbec624a0d7f1d7c8c76902
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-06-16 23:08:49 +0200

    unit test for toString() method as required in issue #33

---
+++ src/test/java/org/aeonbits/owner/ToStringTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2013, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package org.aeonbits.owner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Luigi R. Viggiano
+ */
+public class ToStringTest {
+
+    interface MyConfig extends Config {
+        @Key("max.threads")
+        @DefaultValue("25")
+        int maxThreads();
+
+        @Key("max.folders")
+        @DefaultValue("99")
+        int maxFolders();
+
+        @Key("default.name")
+        @DefaultValue("untitled")
+        String defaultName();
+    }
+
+    @Test
+    public void toStringTest() {
+        MyConfig cfg = ConfigFactory.create(MyConfig.class);
+        assertEquals("{default.name=untitled, max.folders=99, max.threads=25}", cfg.toString());
+    }
+}
