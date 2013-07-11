653ef644dd59805b72b0caf0159f67bb254fe5a9
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-11 22:45:23 +0200

    improved scripts

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 APP_DIR="$(dirname $0)/.."
 cd $APP_DIR
-sed -i \~ 's|url: http://owner.aeonbits.org|#url: http://owner.aeonbits.org|' _config.yml 
-jekyll -w serve 
-mv _config.yml~ _config.yml
+sed 's|url: http://owner.aeonbits.org|#url: http://owner.aeonbits.org|' _config.yml > _serve.yml
+jekyll serve -w --config _serve.yml
+rm _serve.yml
