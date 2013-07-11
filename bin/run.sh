6ea2f288c419ddc24308d2591d1b86efcf3e0a42
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-11 22:21:00 +0200

    improved local startup script

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+APP_DIR="$(dirname $0)/.."
+cd $APP_DIR
+sed -i \~ 's|url: http://owner.aeonbits.org|#url: http://owner.aeonbits.org|' _config.yml 
 jekyll -w serve 
+mv _config.yml~ _config.yml
