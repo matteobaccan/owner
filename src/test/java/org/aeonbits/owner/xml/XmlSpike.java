db2a759ddc3ff8e95948bc9bc42508b23bfc2c9b
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-18 20:46:41 +0200

    XmlSpike.java

--- src/test/java/org/aeonbits/owner/xml/XmlSpike.java
+++ src/test/java/org/aeonbits/owner/xml/XmlSpike.java
@@ -12,52 +12,111 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Properties;
+import java.util.Scanner;
 import java.util.Stack;
 
 /**
  * @author Luigi R. Viggiano
  */
 public class XmlSpike {
-    public void testParse() throws ParserConfigurationException, SAXException, IOException {
+    public static class StringInputStream extends InputStream {
+        private String string;
+        private int i;
+
+        public StringInputStream(String string) {
+            this.string = string;
+        }
+
+        @Override
+        public int read() throws IOException {
+            if (i < string.length())
+                return string.charAt(i++);
+            else
+                return -1;
+        }
+    }
+    static class XmlToPropsHandler extends DefaultHandler {
+        private final PrintWriter writer;
+        private final Stack<String> paths = new Stack<String>();
+
+        public XmlToPropsHandler(PrintWriter writer) {
+            this.writer = writer;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName,
+                                 Attributes attributes) throws SAXException {
+            String path = (paths.size() == 0) ? qName : paths.peek() + "." + qName;
+            paths.push(path);
+            for (int i = 0; i < attributes.getLength(); i++) {
+                String attrName = attributes.getQName(i);
+                String attrValue = attributes.getValue(i);
+                writer.println(path + "." + attrName + "=" + attrValue);
+            }
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            String value = new String(ch, start, length).trim();
+            if (!value.isEmpty())
+                writer.println(paths.peek() + "=" + new String(ch, start, length).trim());
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            paths.pop();
+        }
+    }
+
+    public static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+
+    public static void main(String[] args) throws Exception {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser = factory.newSAXParser();
 
-        InputStream in = getClass().getResourceAsStream("Config.xml");
-        DefaultHandler h = new DefaultHandler() {
-            Stack<String> paths = new Stack<String>();
-            
-            @Override
-            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-                String path = (paths.size() == 0) ? qName : paths.peek() + "." + qName;
-                paths.push(path);
-                for (int i = 0; i < attributes.getLength(); i++) {
-                    String attrName = attributes.getQName(i);
-                    String attrValue = attributes.getValue(i);
-                    System.out.println(path + "." + attrName + "=" + attrValue);
-                }
-            }
+        InputStream in = XmlSpike.class.getResourceAsStream("Config.xml");
 
-            @Override
-            public void characters(char[] ch, int start, int length) throws SAXException {
-                String value = new String(ch, start, length).trim();
-                if (!value.isEmpty())
-                    System.out.println(paths.peek() + "=" + new String(ch, start, length).trim());
-            }
+        String input = convertStreamToString(in);
 
-            @Override
-            public void endElement(String uri, String localName, String qName) throws SAXException {
-                paths.pop();
-            }
-        };
-        parser.parse(in, h);
-    }
+        System.out.println("input:\n" + input);
 
-    public static void main(String[] args) throws Exception {
-        new XmlSpike().testParse();
+        StringWriter output = new StringWriter();
+        PrintWriter pw = new PrintWriter(output);
+        XmlToPropsHandler h = new XmlToPropsHandler(pw);
+        parser.parse(new StringInputStream(input), h);
+        pw.flush();
+
+        System.out.println("output:\n" + output);
+
+        File file = new File("target/test-resources/props.xml");
+        file.getParentFile().mkdirs();
+
+
+        Properties props = new Properties();
+        props.load(new StringInputStream(output.toString()));
+
+        ByteArrayOutputStream propertiesxmlformat = new ByteArrayOutputStream();
+        props.storeToXML(propertiesxmlformat, "test");
+        String propsXmlString = propertiesxmlformat.toString();
+
+        
+        System.out.println("java xml properties format:" + propsXmlString);
+
+        boolean isJavaProp = propsXmlString.contains("http://java.sun.com/dtd/properties.dtd");
+        
+        System.out.println("isJavaProp: " + isJavaProp);
     }
 }
