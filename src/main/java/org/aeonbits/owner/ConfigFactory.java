7c5c36bd81fac28cfdd22ebd6b72625e688f1d01
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-22 01:11:18 +0200

    removed wrong TODO tag

--- src/main/java/org/aeonbits/owner/ConfigFactory.java
+++ src/main/java/org/aeonbits/owner/ConfigFactory.java
@@ -28,8 +28,6 @@ import static org.aeonbits.owner.Util.prohibitInstantiation;
  */
 public abstract class ConfigFactory {
 
-    // TODO: extract this into a class HotReloadScheduler that allows scheduling only once per class,
-    // and keeps a reference to the command scheduled and - possibly - allows the user to dispose the scheduled command.
     private static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
         public Thread newThread(Runnable r) {
             Thread result = new Thread(r);
