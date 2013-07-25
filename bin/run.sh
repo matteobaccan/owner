107e77b29a989aac2c4158751808eda332189aee
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-25 20:48:45 +0200

    Updating to #a5c064a92acc154ecfc80dfde359180071bbb5a0.

--- bin/run.sh
+++ bin/run.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 APP_DIR="$(dirname $0)/.."
-cd $APP_DIR
+cd ${APP_DIR}
 
 #sed 's|url: http://owner.aeonbits.org|#url: http://owner.aeonbits.org|' _config.yml > _serve.yml
 
-cat > _serve.yml <<-EOF
+SERVE_FILE="_serve-${RANDOM}.yml"
+cat > ${SERVE_FILE} <<-EOF
 	url: http://localhost:4000
 EOF
 
-jekyll serve -w --config _config.yml,_serve.yml $@
-rm _serve.yml
+jekyll serve -w --config _config.yml,${SERVE_FILE} $@
+rm ${SERVE_FILE}
