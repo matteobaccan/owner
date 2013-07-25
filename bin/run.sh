c8da33df780f4d6f2a8d834b361d26cbdda7dab4
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-25 22:24:21 +0200

    Updating to 71c2ac2326eb38aebcb3c9c17f0a6b033447bd80.

--- bin/run.sh
+++ bin/run.sh
@@ -2,8 +2,6 @@
 APP_DIR="$(dirname $0)/.."
 cd ${APP_DIR}
 
-#sed 's|url: http://owner.aeonbits.org|#url: http://owner.aeonbits.org|' _config.yml > _serve.yml
-
 SERVE_FILE="_serve-${RANDOM}.yml"
 cat > ${SERVE_FILE} <<-EOF
 	url: http://localhost:4000
