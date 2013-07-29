5ad20c582fe232565c32476c020ab29a3dd13c01
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-29 23:31:19 +0200

    javadocs

--- src/main/java/org/aeonbits/owner/Accessible.java
+++ src/main/java/org/aeonbits/owner/Accessible.java
@@ -97,9 +97,45 @@ public interface Accessible {
      */
     String getProperty(String key, String defaultValue);
 
-    //TODO: javadocs
+    /**
+     * Emits an XML document representing all of the properties contained
+     * in this table.
+     *
+     * <p> An invocation of this method of the form <tt>props.storeToXML(os,
+     * comment)</tt> behaves in exactly the same way as the invocation
+     * <tt>props.storeToXML(os, comment, "UTF-8");</tt>.
+     *
+     * @param os the output stream on which to emit the XML document.
+     * @param comment a description of the property list, or <code>null</code>
+     *        if no comment is desired.
+     * @throws IOException if writing to the specified output stream
+     *         results in an <tt>IOException</tt>.
+     * @throws NullPointerException if <code>os</code> is null.
+     * @throws ClassCastException  if this <code>Properties</code> object
+     *         contains any keys or values that are not
+     *         <code>Strings</code>.
+     * @since 1.0.5
+     */
     void storeToXML(OutputStream os, String comment) throws IOException;
 
-    //TODO: javadocs
+    /**
+     * Returns a set of keys in this property list
+     * including distinct keys in the default property list if a key
+     * of the same name has not already been found from the main
+     * properties list.
+     * <p>
+     * The returned set is not backed by the <tt>Properties</tt> object.
+     * Changes to this <tt>Properties</tt> are not reflected in the set,
+     * or vice versa.
+     *
+     * @return  a set of keys in this property list, including the keys in the
+     *          default property list.
+     * @throws  ClassCastException if any key in this property list
+     *          is not a string.
+     * @see     java.util.Properties#defaults
+     * @see     java.util.Properties#stringPropertyNames()
+     * @see     java.util.Properties#propertyNames()
+     * @since   1.0.5
+     */
     Set<String> propertyNames();
 }
