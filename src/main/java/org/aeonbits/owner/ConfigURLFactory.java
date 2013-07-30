d5d7bb49ceffdd9f04e4afa786c47b62c287393c
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-07-30 14:15:13 +0200

    added missing copyright; ConfigURLFactory.toClasspathURLSpec() made package accessible

--- src/main/java/org/aeonbits/owner/ConfigURLFactory.java
+++ src/main/java/org/aeonbits/owner/ConfigURLFactory.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2013, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
 package org.aeonbits.owner;
 
 import java.net.MalformedURLException;
@@ -29,12 +37,12 @@ class ConfigURLFactory {
         }
         return new URL(url.getProtocol(), url.getHost(), url.getPort(), expand(url.getPath()));
     }
-    
+
     private String expand(String path) {
         return expander.expand(path);
     }
 
-    public String toClasspathURLSpec(String name) {
+    String toClasspathURLSpec(String name) {
         return CLASSPATH_PROTOCOL + name.replace('.', '/');
     }
 }
--- src/test/java/org/aeonbits/owner/loadstrategies/LoadStrategyTestBase.java
+++ src/test/java/org/aeonbits/owner/loadstrategies/LoadStrategyTestBase.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2013, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
 package org.aeonbits.owner.loadstrategies;
 
 import org.hamcrest.BaseMatcher;
