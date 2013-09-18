fdd29cb49f08983e51cfb2e8d135882e5c4762f5
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-09-18 10:40:41 +0200

    added test to reproduce issue #50

---
+++ src/test/java/org/aeonbits/owner/VariablesExpanderForTest.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2013, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package org.aeonbits.owner;
+
+import java.util.Properties;
+
+/**
+ * @author Luigi R. Viggiano
+ */
+public class VariablesExpanderForTest extends VariablesExpander {
+    public VariablesExpanderForTest(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public String expand(String path) {
+        return super.expand(path);
+    }
+}
