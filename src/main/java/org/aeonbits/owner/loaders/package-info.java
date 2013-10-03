18d9c0bd39cc8fa4f5eac82beb5ee699683213f5
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2013-10-03 10:11:31 +0200

    javadocs

---
+++ src/main/java/org/aeonbits/owner/loaders/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2013, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+/**
+ * Provides interfaces and classes to allow OWNER to load properties from several file formats.
+ */
+package org.aeonbits.owner.loaders;
