bf7ec326b906cc5dac8f76aa514c5bffaa702ea7
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2014-04-09 12:32:12 +0200

    static member instance CACHE made final; improved javadocs

--- owner/src/main/java/org/aeonbits/owner/ConfigCache.java
+++ owner/src/main/java/org/aeonbits/owner/ConfigCache.java
@@ -13,15 +13,15 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * Utility class to have a cache for Config instances, that can be used as a singleton factory.
+ * Utility class caching Config instances that can be used as Singletons.
  *
- * The <tt>ConfigCache</tt> is designed to be thread safe.
+ * This class is designed to be thread safe.
  *
  * @author Luigi R. Viggiano
  * @since 1.0.6
  */
 public final class ConfigCache {
-    private static ConcurrentMap<Object, Config> CACHE = new ConcurrentHashMap<Object, Config>();
+    private static final ConcurrentMap<Object, Config> CACHE = new ConcurrentHashMap<Object, Config>();
 
     /** Don't let anyone instantiate this class */
     private ConfigCache() {}
@@ -33,8 +33,8 @@ public final class ConfigCache {
      * @param clazz     the interface extending from {@link Config} that you want to instantiate.
      * @param imports   additional variables to be used to resolve the properties.
      * @param <T>       type of the interface.
-     * @return an object implementing the given interface, that can be taken from the cache, which maps methods
-     *         to property values.
+     * @return          an object implementing the given interface, that can be taken from the cache,
+     *                  which maps methods to property values.
      */
     public static <T extends Config> T getOrCreate(Class<? extends T> clazz, Map<?, ?>... imports) {
         return getOrCreate(ConfigFactory.INSTANCE, clazz, clazz, imports);
@@ -47,8 +47,8 @@ public final class ConfigCache {
      * @param clazz     the interface extending from {@link Config} that you want to instantiate.
      * @param imports   additional variables to be used to resolve the properties.
      * @param <T>       type of the interface.
-     * @return an object implementing the given interface, that can be taken from the cache, which maps methods
-     *         to property values.
+     * @return          an object implementing the given interface, that can be taken from the cache,
+     *                  which maps methods to property values.
      */
     public static <T extends Config> T getOrCreate(Factory factory, Class<? extends T> clazz, Map<?, ?>... imports) {
         return getOrCreate(factory, clazz, clazz, imports);
@@ -56,13 +56,14 @@ public final class ConfigCache {
 
     /**
      * Gets from the cache or create, an instance of the given class using the given imports.
+     * The factory used to create new instances is the static {@link ConfigFactory#INSTANCE}.
      *
      * @param key       the key object to be used to identify the instance in the cache.
      * @param clazz     the interface extending from {@link Config} that you want to instantiate.
      * @param imports   additional variables to be used to resolve the properties.
      * @param <T>       type of the interface.
-     * @return an object implementing the given interface, that can be taken from the cache, which maps methods
-     *         to property values.
+     * @return          an object implementing the given interface, that can be taken from the cache,
+     *                  which maps methods to property values.
      */
     public static <T extends Config> T getOrCreate(Object key, Class<? extends T> clazz, Map<?, ?>... imports) {
         return getOrCreate(ConfigFactory.INSTANCE, key, clazz, imports);
@@ -76,8 +77,8 @@ public final class ConfigCache {
      * @param clazz     the interface extending from {@link Config} that you want to instantiate.
      * @param imports   additional variables to be used to resolve the properties.
      * @param <T>       type of the interface.
-     * @return an object implementing the given interface, that can be taken from the cache, which maps methods
-     *         to property values.
+     * @return          an object implementing the given interface, that can be taken from the cache,
+     *                  which maps methods to property values.
      */
     public static <T extends Config> T getOrCreate(Factory factory, Object key,
                                                    Class<? extends T> clazz, Map<?, ?>[] imports) {
@@ -93,7 +94,7 @@ public final class ConfigCache {
      *
      * @param key       the key object to be used to identify the instance in the cache.
      * @param <T>       type of the interface.
-     * @return the {@link Config} object from the cache if exists, or <tt>null</tt> if it doesn't.
+     * @return          the {@link Config} object from the cache if exists, or <tt>null</tt> if it doesn't.
      */
     public static <T extends Config> T get(Object key) {
         return (T) CACHE.get(key);
@@ -105,8 +106,8 @@ public final class ConfigCache {
      * @param key       the key object to be used to identify the instance in the cache.
      * @param instance  the instance of the {@link Config} object to be stored into the cache.
      * @param <T>       type of the interface.
-     * @return the previous value associated with the specified key, or
-     *         <tt>null</tt> if there was no mapping for the key.
+     * @return          the previous value associated with the specified key, or
+     *                  <tt>null</tt> if there was no mapping for the key.
      */
     public static <T extends Config> T add(Object key, T instance) {
         return (T) CACHE.putIfAbsent(key, instance);
@@ -129,9 +130,9 @@ public final class ConfigCache {
      * <p>The cache will not contain the instance for the specified key once the
      * call returns.
      *
-     * @param   key key whose instance is to be removed from the cache.
-     * @return the previous instance associated with <tt>key</tt>, or
-     *         <tt>null</tt> if there was no instance for <tt>key</tt>.
+     * @param key   key whose instance is to be removed from the cache.
+     * @return      the previous instance associated with <tt>key</tt>, or
+     *              <tt>null</tt> if there was no instance for <tt>key</tt>.
      */
     public static <T extends Config> T remove(Object key) {
         return (T) CACHE.remove(key);
