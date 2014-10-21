2e90a823cacc37740cfb0f2c61c0d9df631b6732
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2014-10-21 12:50:52 +0200

    more tests for java8 support

--- owner-java8/src/test/java/org/aeonbits/owner/java8/ConfigWithDefaultMethods.java
+++
@@ -1,9 +0,0 @@
-package org.aeonbits.owner.java8;
-
-import org.aeonbits.owner.Config;
-
-public interface ConfigWithDefaultMethods extends Config {
-    default public Integer sum(Integer a, Integer b) {
-        return Integer.sum(a, b);
-    }
-}
--- owner-java8/src/test/java/org/aeonbits/owner/java8/Java8Test.java
+++ owner-java8/src/test/java/org/aeonbits/owner/java8/Java8Test.java
@@ -8,6 +8,7 @@
 package org.aeonbits.owner.java8;
 
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,10 +18,26 @@ import static org.junit.Assert.assertEquals;
  */
 public class Java8Test {
 
+    private ConfigWithJava8Features cfg;
+
+    @Before
+    public void before() {
+        cfg = ConfigFactory.create(ConfigWithJava8Features.class);
+    }
 
     @Test
     public void defaultMethodsShouldWork() {
-        ConfigWithDefaultMethods cfg = ConfigFactory.create(ConfigWithDefaultMethods.class);
         assertEquals(new Integer(8), cfg.sum(3, 5));
+    }
+
+
+    @Test
+    public void staticMethodsShouldWork() {
+        assertEquals(3, ConfigWithJava8Features.min(3, 10));
+    }
+
+    @Test
+    public void regularInterfaceMethodsShouldWork() {
+        assertEquals(100, cfg.oneHundred());
     }
 }
