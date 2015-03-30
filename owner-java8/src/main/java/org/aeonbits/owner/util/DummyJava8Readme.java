0d7d3fd3729fc1ada05a404c188ec25062091819
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2015-03-30 21:38:27 +0200

    working-around bugs on sonatype that prevents releasing

---
+++ owner-java8/src/main/java/org/aeonbits/owner/util/DummyJava8Readme.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2012-2015, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package org.aeonbits.owner.util;
+
+/**
+ * Dummy class to work around bugs:
+ *
+ *  - https://issues.sonatype.org/browse/OSSRH-3723 
+ *  - https://issues.sonatype.org/browse/OSSRH-637
+ *
+ * This class is useless; please just ignore it.
+ * It is just needed to allow the release on SonaType, since validation is failing for missing javadocs. 
+ *
+ * @author Luigi R. Viggiano
+ */
+public class DummyJava8Readme {
+}
