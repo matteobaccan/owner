314c1416b2f920d099a01f3f2bf3ce1ae05ef48a
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2015-03-30 23:03:04 +0200

    cleanup

---
+++ owner-java8/src/main/java/org/aeonbits/owner/dummy/DummyJava8Readme.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2012-2015, Luigi R. Viggiano
+ * All rights reserved.
+ *
+ * This software is distributable under the BSD license.
+ * See the terms of the BSD license in the documentation provided with this software.
+ */
+
+package org.aeonbits.owner.dummy;
+
+/**
+ * Dummy class to work around bugs:
+ *
+ * <ul>
+ *  <li><a href="https://issues.sonatype.org/browse/OSSRH-3723">OSSRH-3723</a></li>
+ *  <li><a href="https://issues.sonatype.org/browse/OSSRH-637">OSSRH-637</a></li>
+ * </ul>
+ *
+ * <p>This class is useless; please just ignore it.</p>
+ * <p>It is just needed to allow the release on SonaType, since validation is failing for missing javadocs.</p>
+ *
+ * @author Luigi R. Viggiano
+ */
+public class DummyJava8Readme {
+}
