64424ca482bdf3f2e6eb6450747fad7b2378f17f
Kevin LeBlanc <kele@hms.se>
2015-08-12 20:39:29 +0200

    Add list() to ConfigCache
    ConfigCache is a great way to centralise configuration for various
    parts of an application. This commit adds a list() method to the
    ConfigCache class, which lists the keys for all configurations
    present in the cache. This allows the entire application
    configuration to be inspected (e.g. for debugging) without the need
    for storing cache keys elsewhere.
    
    Change-Id: I3c5188f15435241dd360597b3ae79c410a845f27

--- owner/src/main/java/org/aeonbits/owner/ConfigCache.java
+++ owner/src/main/java/org/aeonbits/owner/ConfigCache.java
@@ -8,7 +8,9 @@
 
 package org.aeonbits.owner;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -113,6 +115,19 @@ public final class ConfigCache {
     @SuppressWarnings("unchecked")
     public static <T extends Config> T add(Object key, T instance) {
         return (T) CACHE.putIfAbsent(key, instance);
+    }
+
+    /**
+     * Lists the key objects for all configuration instances present in the cache.
+     *
+     * @return a set containing the key objects for all instance in the cache.
+     */
+    public static Set<Object> list() {
+        // Create a new Set to ensure that the caller does not modify the contents of our
+        // private map via the result of this call. The key objects themselves are the same
+        // as those contained in the private map, which means that if they are mutable, the
+        // caller will be able to affect the contents of the map (albeit only the keys).
+        return new HashSet<Object>(CACHE.keySet());
     }
 
     /**
