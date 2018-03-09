3acb13de7caad8614563b0e40bb61d3799cc534e
Luigi R. Viggiano <luigi.viggiano@newinstance.it>
2018-03-09 11:42:17 +0100

    Updating to 590257b.

---
+++ apidocs/latest/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"org.aeonbits.owner"},{"l":"org.aeonbits.owner.converters"},{"l":"org.aeonbits.owner.crypto"},{"l":"org.aeonbits.owner.dummy"},{"l":"org.aeonbits.owner.event"},{"l":"org.aeonbits.owner.examples"},{"l":"org.aeonbits.owner.loaders"},{"l":"org.aeonbits.owner.util"},{"l":"org.aeonbits.owner.util.bytesize"}]
